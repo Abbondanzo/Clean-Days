fb11c579c72c12dbfe29eaf803daa5f1978ed057
Abbondanzo <peter@abbondanzo.com>
2024-10-01 23:02:33 -0400

    Metro config changes

--- metro.config.js
+++ metro.config.js
@@ -5,23 +5,15 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname, {
   isCSSEnabled: true,
 });
-
-// This is not needed for NativeWind, it is configuration for Metro to understand monorepos
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
-config.watchFolders = [workspaceRoot];
-config.resolver.disableHierarchicalLookup = true;
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, 'node_modules'),
-  path.resolve(workspaceRoot, 'node_modules'),
-];
+module.exports = config;
 
 // 2. Enable NativeWind
-const { withNativeWind } = require('nativewind/metro');
-module.exports = withNativeWind(config, {
-  // 3. Set `input` to your CSS file with the Tailwind at-rules
-  input: 'global.css',
-  // This is optional
-  projectRoot,
-  inlineRem: false,
-});
+// const { withNativeWind } = require('nativewind/metro');
+// module.exports = withNativeWind(config, {
+
+//   // 3. Set `input` to your CSS file with the Tailwind at-rules
+//   input: 'global.css',
+//   // This is optional
+//   projectRoot: __dirname,
+//   inlineRem: false,
+// });
