fd3d702458d4e019f496c1681d13a7c301342bcb
Abbondanzo <peter@abbondanzo.com>
2025-01-04 15:43:29 -0500

    Remove redundant color scheme import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ import { useEffect } from 'react';
 import { Platform } from 'react-native';
 import 'react-native-reanimated';
 import { ThemeProvider } from '../components/theme/ThemeProvider';
-import { useColorScheme } from '../hooks/useColorScheme';
 import { Root } from '../ide/Root';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -31,7 +30,6 @@ const MobileLayout = () => {
 };
 
 export default function RootLayout() {
-  const { colorScheme } = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
